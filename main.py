5e23a33e9cc3dc4e61a764c191dab714a0e02147
TBargos <bargos.work@gmail.com>
2023-08-04 18:21:06 +0300

    Добавлена возможность редактирования списка разрешённых спецсимволов

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ import random
 import string
 
 q_values = random.randint(6, 12) # длина пароля
-print(q_values)
-all = string.ascii_letters + string.digits + string.punctuation # массив символов для сборки пароля
-
+option_symbols = "_-" # разрешённые спецсимволы. Отличаются на разных серверах
+all = string.ascii_letters + string.digits + option_symbols # массив символов для сборки пароля
 password = ''
 for i in range(q_values): # цикл посимвольной сборки парольной строки
     random_letter = all[random.randint(0, len(all) - 1)]
     password += random_letter
-
 print(password)
-print(len(password))
-
-# нужно как-то приспособить программу под разные требования серверов
